3e795e7953c9b076438305754cd61b9a36b125f6
ymwsimon <ymwsimon@gmail.com>
2025-02-20 10:54:52 +0000

    change vial uid

--- keyboards/ydkb/unicore_f1/keymaps/agar_vial/config.h
+++ keyboards/ydkb/unicore_f1/keymaps/agar_vial/config.h
@@ -6,7 +6,7 @@
 #define PRODUCT     Agar Keyboard (FW_VER)
 #define DYNAMIC_KEYMAP_LAYER_COUNT    8
 #define FLASH_KEYMAP_COUNT    2
-#define VIAL_KEYBOARD_UID    {0x2E, 0xE6, 0x0E, 0x23, 0x34, 0xEF, 0x99, 0x37}
+#define VIAL_KEYBOARD_UID    {0x2E, 0xE6, 0x0E, 0x23, 0x34, 0xEF, 0x99, 0x57}
 
 #undef  RGBLIGHT_LIMIT_VAL
 #define RGBLIGHT_LIMIT_VAL    128
